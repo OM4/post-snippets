ef74bc3bb74f65b8ffaf013c183a043dcbf372ee
Johan Steen <artstorm@gmail.com>
2013-02-23 02:05:53 +0700

    get_post_snippet() now calls a static method.

--- post-snippets.php
+++ post-snippets.php
@@ -763,7 +763,7 @@ function edOpenPostSnippets(myField) {
 	 * @return	string
 	 *			The Snippet
 	 */
-	public function get_snippet( $snippet_name, $snippet_vars = '' )
+	public static function get_snippet( $snippet_name, $snippet_vars = '' )
 	{
 		$snippets = get_option( self::OPTION_DB_KEY );
 		for ($i = 0; $i < count($snippets); $i++) {
@@ -862,7 +862,9 @@ add_action('plugins_loaded', array('PostSnippets', 'getInstance'));
  * @return	string
  *			The Snippet
  */
-function get_post_snippet( $snippet_name, $snippet_vars = '' ) {
-	global $post_snippets;
-	return $post_snippets->get_snippet( $snippet_name, $snippet_vars );
+function get_post_snippet($snippet_name, $snippet_vars = '')
+{
+
+	// global $post_snippets;
+	return PostSnippets::get_snippet($snippet_name, $snippet_vars);
 }
